b019201656662cdbb50d7e49a9bbbc228ddbedc5
zhouyw <zhouyiwei@travelsky.com>
2020-01-07 15:48:50 +0800

    parent上传

---
+++ doudou-user/src/main/java/com/zyw/doudou/user/DoudouUserApplication.java
@@ -0,0 +1,13 @@
+package com.zyw.doudou.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoudouUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoudouUserApplication.class, args);
+    }
+
+}
---
+++ doudou-user/src/test/java/com/zyw/doudou/user/DoudouUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyw.doudou.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoudouUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
